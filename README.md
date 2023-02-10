07dbf53e057f10844ff43aa6abdd19324d2f8ddb
mika <mikagivner123@gmail.com>
2023-02-10 18:57:54 +0200

    population

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # World-Population
 
+<p>difficulties:</p>
+<ul>
+<li>to access to the API of the cities</li>
+<li>to reset the canvas in every click to get new graph</li>
+</ul>
+
 <p>planning project:</p>
 <img src="./assets/plane_project.png"/>
--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let countriesButtons = document.querySelector(".countriesButtons");
 let continentsButtons = document.querySelector(".continentsButtons");
+let continents = document.querySelector("#continents");
+
 const ctx = document.getElementById("myChart");
 let arrOfContinentsCountry = [];
 let arrOfPopulationsCountry = [];
+let arrOfCities = [];
+let arrOfPopulationsCities = [];
 let isChart = false;
 let myChart;
 continentsButtons.addEventListener("click", getContinent);
 countriesButtons.addEventListener("click", getCountry);
 
 async function getContinent(e) {
+  countriesButtons.style.visibility = "visible";
   arrOfContinentsCountry = [];
   arrOfPopulationsCountry = [];
+  arrOfCities = [];
+  arrOfPopulationsCities = [];
 
   countriesButtons.innerHTML = " ";
   try {
@@ -22,25 +29,54 @@ async function getContinent(e) {
     objContinentData = await continentData.json();
     console.log(objContinentData);
     objContinentData.forEach((oCD) => {
-      arrOfContinentsCountry.push(oCD.altSpellings[1]);
+      arrOfContinentsCountry.push(oCD.name.common);
       arrOfPopulationsCountry.push(oCD.population);
       let btnOfCountry = document.createElement("button");
       btnOfCountry.classList.add("btnContinent");
-      btnOfCountry.innerText = `${oCD.altSpellings[1]}`;
+      btnOfCountry.innerText = `${oCD.name.common}`;
       countriesButtons.append(btnOfCountry);
     });
   } catch (err) {
     console.error(err);
   }
 
-  await draw();
+  await draw(arrOfContinentsCountry, arrOfPopulationsCountry);
 
   //ctx.appendChild(Chart);
 }
 
-async function getCountry(event) {}
+async function getCountry(event) {
+  countriesButtons.style.visibility = "none";
 
-async function draw() {
+  const res = await fetch(
+    "https://countriesnow.space/api/v0.1/countries/population/cities/filter",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        limit: 1000,
+        order: "asc",
+        orderBy: "name",
+        country: `${event.target.innerText}`,
+      }),
+    }
+  );
+  if (!res.ok) throw new Error("error");
+  const data1 = await res.json();
+  console.log(data1.data[1]);
+  data1.data.forEach((city) => {
+    arrOfCities.push(city.city);
+    arrOfPopulationsCities.push(city.populationCounts[0].value);
+  });
+  await draw(arrOfCities, arrOfPopulationsCities);
+  console.log(arrOfCities);
+  console.log(arrOfPopulationsCities);
+}
+
+async function draw(arrOfContinentsCountry, arrOfPopulationsCountry) {
   if (isChart === true) {
     myChart.destroy();
   }
